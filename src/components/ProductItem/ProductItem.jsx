a2b17dd8551bccf45de68c82c302d47e1ab1245d
MarkEmdin <emdinmark@gmail.com>
2022-11-08 19:10:49 +0200

    img

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -7,7 +7,7 @@ const ProductItem = ({product, className}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}> <img src="https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg" height="100"   alt="котик"/></div>
+            <div className={'img'}> <img src={product.image_url} height="100"   alt="котик"/></div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <Button className={'add-btn'}>
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,14 +3,14 @@ import './ProductList.css'
 import ProductItem from "../ProductItem/ProductItem";
 
 const products = [
-    {id: '1', title: 'Джинсы', description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Шкаф',  description: 'Два отсева, самовывоз'},
-    {id: '3', title: 'Кровать',  description: 'Всего одна ножка, подходит если вас зовут Семен'},
-    {id: '4', title: 'Стул',  description: 'Икеа, почти новый'},
-    {id: '5', title: 'Кровать + матрасс',  description: 'Съезжаем, отдадим в хорошие руки'},
-    {id: '6', title: 'Чайник',  description: 'просто чайник'},
-    {id: '7', title: 'Ракетка для настольного тенниса',  description: 'Сын играл раньше проффессионально, сейчас перешел в балет'},
-    {id: '8', title: 'Куртка',  description: 'Зимняя, теплая'},
+    {id: '1', title: 'Джинсы', description: 'Синего цвета, прямые',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
+    {id: '2', title: 'Шкаф',  description: 'Два отсева, самовывоз',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
+    {id: '3', title: 'Кровать',  description: 'Всего одна ножка, подходит если вас зовут Семен',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
+    {id: '4', title: 'Стул',  description: 'Икеа, почти новый',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
+    {id: '5', title: 'Кровать + матрасс',  description: 'Съезжаем, отдадим в хорошие руки',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
+    {id: '6', title: 'Чайник',  description: 'просто чайник',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
+    {id: '7', title: 'Ракетка для настольного тенниса',  description: 'Сын играл раньше проффессионально, сейчас перешел в балет',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
+    {id: '8', title: 'Куртка',  description: 'Зимняя, теплая',image_url:'https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg'},
 ]
 
 const ProductList = () => {
