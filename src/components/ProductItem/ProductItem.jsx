a4064338a1ba2d2778b24285cb9a7a60b83e7ebe
MarkEmdin <emdinmark@gmail.com>
2022-11-08 18:52:29 +0200

    img

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -7,8 +7,8 @@ const ProductItem = ({product, className}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}><a href="sample.html"><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftheoryandpractice.ru%2Fposts%2F10536-molotch&psig=AOvVaw0RwyhMRGEaqzlTAIH1ps2O&ust=1668008538445000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCLClj5X2nvsCFQAAAAAdAAAAABAE" width="50"
-                                                               height="50" alt="Пример"/></a></div>
+            <div className={'img'}> <img src="https://mobimg.b-cdn.net/v3/fetch/ad/ad9ecd9981913c4af8002ef2937e64eb.jpeg?h=900&r=0.5"
+                                         alt="панда на дереве"/></div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <Button className={'add-btn'}>
