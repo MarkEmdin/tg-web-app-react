a9edd96b096270f19d65ffbe6747bfce2dd5b2d1
MarkEmdin <emdinmark@gmail.com>
2022-11-09 11:50:01 +0200

    db connect

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}> <img src={product.image_url} height="100"   alt={product.title}/></div>
+            <div className={'img'}> <img src={product.picture_url} height="100"   alt={product.title}/></div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <Button className={'add-btn'} onClick={onAddHandler}>
